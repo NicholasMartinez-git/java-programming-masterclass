9f34f23d3cbeb864692c3f557eca825f2acafbb2
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-07-29 23:01:33 -0500

    Feat: Update PrimitiveChallenge
    Added part 2 of the challenge

--- src/masterclass/learnprogramming/PrimitiveTypesChallenge.java
+++ src/masterclass/learnprogramming/PrimitiveTypesChallenge.java
@@ -1,14 +1,26 @@
-/* Section 3 : Video 28 */
+
+/* Part 1:
+ Section 3 : Video 28 */
+
+/* Part 2:
+ Section 3 : Video 30 */
 
 package masterclass.learnprogramming;
 
 public class PrimitiveTypesChallenge {
 
     public static void main(String[] args) {
+        // Part 1:
         byte myByte = 20;
         short myShort = 3006;
         int myInt = 12050;
         long myLong = 50000L + 10L * (myByte + myShort + myInt);
         System.out.println(myLong);
+
+        // Part 2:
+        double pounds = 59d;
+        double kilograms = 0.45359237d;
+        double convertedKilograms = pounds * kilograms;
+        System.out.println("Converted kilograms = " + convertedKilograms);
     }
 }
