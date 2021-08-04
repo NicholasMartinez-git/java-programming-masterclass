18382ad259d55c0f22dc7f11af3f550f3345e0d9
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-04 14:54:32 -0500

    feat: Create OperatorPrecedenceAndOperatorChallenge

---
+++ src/masterclass/learnprogramming/OperatorPrecedenceAndOperatorChallenge.java
@@ -0,0 +1,17 @@
+package masterclass.learnprogramming;
+
+public class OperatorPrecedenceAndOperatorChallenge {
+    public static void main(String[] args) {
+        double x = 20.00d;
+        double y = 80.00d;
+        double z = (x + y) * 100.00d;
+        System.out.println("z = " + z);
+        double remainder = z % 40.00d;
+        System.out.println("Remainder = " + remainder);
+        boolean isNoRemainder = remainder == 0; // Simplification of (remainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+    }
+}
