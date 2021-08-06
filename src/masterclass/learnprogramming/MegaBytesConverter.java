5c43aae23da0bee3a2bc7b75703088fa56103d76
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-06 13:58:11 -0500

    feat: Complete MegaBytesConverter

--- src/masterclass/learnprogramming/MegaBytesConverter.java
+++ src/masterclass/learnprogramming/MegaBytesConverter.java
@@ -2,6 +2,19 @@ package masterclass.learnprogramming;
 
 public class MegaBytesConverter {
     public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+    }
 
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        // 1 MB = 1024 KB
+        int megaBytes = kiloBytes / 1024;
+        int remainingKiloBytes = kiloBytes % 1024;
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainingKiloBytes + " KB");
+        }
     }
 }
