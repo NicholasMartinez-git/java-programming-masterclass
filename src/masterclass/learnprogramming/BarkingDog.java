8f238c17c0992eb37bf4b969333a5817d5999176
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-06 14:44:23 -0500

    feat: Complete BarkingDog

--- src/masterclass/learnprogramming/BarkingDog.java
+++ src/masterclass/learnprogramming/BarkingDog.java
@@ -2,6 +2,17 @@ package masterclass.learnprogramming;
 
 public class BarkingDog {
     public static void main(String[] args) {
+        System.out.println(shouldWakeUp(true, 1)); // true
+        System.out.println(shouldWakeUp(false, 2)); // false
+        System.out.println(shouldWakeUp(true, 8)); // false
+        System.out.println(shouldWakeUp(true, -1)); // false
+    }
 
+    public static boolean shouldWakeUp(boolean isDogBarking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else {
+            return isDogBarking && (hourOfDay < 8 || hourOfDay > 22);
+        }
     }
 }
