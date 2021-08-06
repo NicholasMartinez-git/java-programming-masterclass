cb77c27d72628734bcbc0904bbb2387e25a3d4b1
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-05 22:42:15 -0500

    feat: Create SpeedConverter

---
+++ src/masterclass/learnprogramming/SpeedConverter.java
@@ -0,0 +1,28 @@
+/* Section 4 : Coding Exercise 1: Speed Converter */
+
+package masterclass.learnprogramming;
+
+public class SpeedConverter {
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (Math.round(kilometersPerHour / 1.609d) < 0) {
+            return -1;
+        }
+        return Math.round(kilometersPerHour / 1.609d);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+    }
+}
