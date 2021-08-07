a5c98f0efdf93c0de816089178967b39d21e078e
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-07 15:20:31 -0500

    feat: Complete EqualSumChecker

--- src/masterclass/learnprogramming/EqualSumChecker.java
+++ src/masterclass/learnprogramming/EqualSumChecker.java
@@ -2,6 +2,12 @@ package masterclass.learnprogramming;
 
 public class EqualSumChecker {
     public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
 
+    public static boolean hasEqualSum(int num1, int num2, int sum) {
+        return num1 + num2 == sum;
     }
 }
