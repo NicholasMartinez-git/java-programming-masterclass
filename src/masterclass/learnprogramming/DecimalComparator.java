a55cd0bfafe9d08b9269dc5ed611fde1215924bc
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-07 01:40:41 -0500

    feat: Complete DecimalComparator

--- src/masterclass/learnprogramming/DecimalComparator.java
+++ src/masterclass/learnprogramming/DecimalComparator.java
@@ -1,7 +1,20 @@
 package masterclass.learnprogramming;
 
+import java.text.DecimalFormat;
+
 public class DecimalComparator {
     public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
+    }
 
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+        double value1 = (num1 * 1000);
+        double value2 = (num2 * 1000);
+        int castedValue1 = (int) value1;
+        int castedValue2 = (int) value2;
+        return castedValue1 == castedValue2;
     }
 }
