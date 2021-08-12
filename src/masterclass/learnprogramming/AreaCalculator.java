829c82549e3283420c7a633d72df918008e7ec8e
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-12 11:37:33 -0500

    feat: Complete AreaCalculator

--- src/masterclass/learnprogramming/AreaCalculator.java
+++ src/masterclass/learnprogramming/AreaCalculator.java
@@ -3,6 +3,25 @@ package masterclass.learnprogramming;
 public class AreaCalculator {
 
     public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5, 4));
+        System.out.println(area(-1, 4));
+    }
 
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1;
+        }
+        double areaOfACircle = Math.PI * Math.pow(radius, 2);
+        return areaOfACircle;
+    }
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1;
+        }
+        double areaOfARectangle = x * y;
+        return areaOfARectangle;
     }
 }
