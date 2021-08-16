3c7741e5b82d89c2bdc5fdd9b46acc6af2a390b1
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-16 14:41:44 -0500

    feat: Complete NumberOfDaysInMonth

--- src/masterclass/learnprogramming/NumberOfDaysInMonth.java
+++ src/masterclass/learnprogramming/NumberOfDaysInMonth.java
@@ -2,9 +2,53 @@
 
 package masterclass.learnprogramming;
 
+import static masterclass.learnprogramming.LeapYearCalculator.isLeapYear;
+
 public class NumberOfDaysInMonth {
 
     public static void main(String[] args) {
+        System.out.println("The number of days in month = " + getDaysInMonth(1, 2020));
+        System.out.println("The number of days in month = " + getDaysInMonth(2, 2020));
+        System.out.println("The number of days in month = " + getDaysInMonth(2, 2018));
+        System.out.println("The number of days in month = " + getDaysInMonth(-1, 2020));
+        System.out.println("The number of days in month = " + getDaysInMonth(1, -2020));
+    }
 
+    public static int getDaysInMonth(int month, int year) {
+
+        int daysOfMonth;
+
+        if (month < 1 || month > 12 || year < 1 || year > 9999) {
+            return -1;
+        } else if (month == 1) {
+            daysOfMonth = 31;
+        } else if (month == 2) {
+            if (isLeapYear(year)) {
+                daysOfMonth = 29;
+            } else {
+                daysOfMonth = 28;
+            }
+        } else if (month == 3) {
+            daysOfMonth = 31;
+        } else if (month == 4) {
+            daysOfMonth = 30;
+        } else if (month == 5) {
+            daysOfMonth = 31;
+        } else if (month == 6) {
+            daysOfMonth = 30;
+        } else if (month == 7) {
+            daysOfMonth = 31;
+        } else if (month == 8) {
+            daysOfMonth = 31;
+        } else if (month == 9) {
+            daysOfMonth = 30;
+        } else if (month == 10) {
+            daysOfMonth = 31;
+        } else if (month == 11) {
+            daysOfMonth = 30;
+        } else {
+            daysOfMonth = 31;
+        }
+        return daysOfMonth;
     }
 }
