127cb7aabbd504f049e3330c40029914ccdd7ff7
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-17 13:31:43 -0500

    feat: Complete Sum3And5Challenge

---
+++ src/masterclass/learnprogramming/Sum3And5Challenge.java
@@ -0,0 +1,21 @@
+package masterclass.learnprogramming;
+
+public class Sum3And5Challenge {
+    public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 15 == 0) {
+                count++;
+                sum += i;
+                System.out.println(i);
+            }
+
+            if (count == 5) {
+                break;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
