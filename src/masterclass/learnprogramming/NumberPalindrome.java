fc3010664fc43b876507bb063d9fc4cf69fafd09
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-18 16:30:40 -0500

    feat: Complete NumberPalindrome

--- src/masterclass/learnprogramming/NumberPalindrome.java
+++ src/masterclass/learnprogramming/NumberPalindrome.java
@@ -2,8 +2,21 @@
 
 package masterclass.learnprogramming;
 
+import java.util.*;
+
 public class NumberPalindrome {
     public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(112112));
+    }
 
+    public static boolean isPalindrome(int number) {
+        int absoluteNumber = Math.abs(number);
+        String numberString = String.valueOf(absoluteNumber);
+        String[] numberStringArray = numberString.split("");
+        Collections.reverse(Arrays.asList(numberStringArray));
+        int reversedNumber = Integer.parseInt(String.join("", numberStringArray));
+        return absoluteNumber == reversedNumber;
     }
 }
