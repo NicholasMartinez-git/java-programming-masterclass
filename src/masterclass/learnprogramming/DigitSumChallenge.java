dee5a317b5d69a3ddd0f615864312c9ea2ec9262
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-17 22:41:54 -0500

    feat: Complete DigitSumChallenge

--- src/masterclass/learnprogramming/DigitSumChallenge.java
+++ src/masterclass/learnprogramming/DigitSumChallenge.java
@@ -2,8 +2,30 @@
 
 package masterclass.learnprogramming;
 
+import java.util.Arrays;
+
 public class DigitSumChallenge {
     public static void main(String[] args) {
+        System.out.println(sumDigits(-1));
+        System.out.println(sumDigits(9));
+        System.out.println(sumDigits(10));
+        System.out.println(sumDigits(125));
+        System.out.println(sumDigits(1259));
+        System.out.println(sumDigits(12597));
+    }
 
+    public static int sumDigits(int number) {
+        int sumOfAllDigits = 0;
+        String numberString = String.valueOf(number);
+        String[] splitNumberString = numberString.split("");
+        if (number >= 10) {
+            for (int i = 0; i < splitNumberString.length; i++) {
+                int splitNumber = Integer.parseInt(splitNumberString[i]);
+                sumOfAllDigits += splitNumber;
+            }
+            return sumOfAllDigits;
+        } else {
+            return -1;
+        }
     }
 }
