04f011e2b712dd0d7a58db97a877218eabdc3b27
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-18 22:01:17 -0500

    feat: Complete FirstAndLastDigitSum

--- src/masterclass/learnprogramming/FirstAndLastDigitSum.java
+++ src/masterclass/learnprogramming/FirstAndLastDigitSum.java
@@ -3,6 +3,26 @@ package masterclass.learnprogramming;
 public class FirstAndLastDigitSum {
 
     public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-1));
+    }
 
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int sumOfFirstAndLastDigit = 0;
+        String numberString = String.valueOf(number);
+        String[] splitNumberString = numberString.split("");
+        for (int i = 0; i < splitNumberString.length; i++) {
+            int firstDigit = Integer.parseInt(splitNumberString[0]);
+            int lastDigit = Integer.parseInt(splitNumberString[splitNumberString.length - 1]);
+            sumOfFirstAndLastDigit = firstDigit + lastDigit;
+        }
+        return sumOfFirstAndLastDigit;
     }
 }
