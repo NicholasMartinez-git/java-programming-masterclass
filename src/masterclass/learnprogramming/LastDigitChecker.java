5b3f9da038118d92fbb565f03bf09da31e30e162
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-19 16:26:47 -0500

    feat: Update LastDigitChecker

--- src/masterclass/learnprogramming/LastDigitChecker.java
+++ src/masterclass/learnprogramming/LastDigitChecker.java
@@ -4,4 +4,8 @@ public class LastDigitChecker {
     public static void main(String[] args) {
 
     }
+
+    public static boolean isValid(int number) {
+        return number >= 10 && number <= 1000;
+    }
 }
