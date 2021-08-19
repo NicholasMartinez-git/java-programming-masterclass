968209eee5aae6eeaea16f51602e0dbdd621811d
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-19 01:55:26 -0500

    feat: Update SharedDigit

--- src/masterclass/learnprogramming/SharedDigit.java
+++ src/masterclass/learnprogramming/SharedDigit.java
@@ -4,4 +4,15 @@ public class SharedDigit {
     public static void main(String[] args) {
 
     }
+
+    public static boolean hasSharedDigit(int num1, int num2) {
+        if (num1 >= 10 && num1 <= 99 && num2 >= 10 && num2 <= 99) {
+            String numberString1 = String.valueOf(num1);
+            String numberString2 = String.valueOf(num2);
+            String[] splitNumberString1 = numberString1.split("");
+            String[] splitNumberString2 = numberString2.split("");
+        } else {
+            return false;
+        }
+    }
 }
