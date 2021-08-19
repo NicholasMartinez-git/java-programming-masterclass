f00890869601296a58f964b8c35d798d3164dc42
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-18 22:22:03 -0500

    feat: Complete EvenDigitSum

--- src/masterclass/learnprogramming/EvenDigitSum.java
+++ src/masterclass/learnprogramming/EvenDigitSum.java
@@ -2,6 +2,25 @@ package masterclass.learnprogramming;
 
 public class EvenDigitSum {
     public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
 
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int sumOfEvenDigit = 0;
+        String numberString = String.valueOf(number);
+        String[] splitNumberString = numberString.split("");
+        for (int i = 0; i < splitNumberString.length; i++) {
+            int splitNumber = Integer.parseInt(splitNumberString[i]);
+            if (splitNumber % 2 == 0) {
+                sumOfEvenDigit += splitNumber;
+            }
+        }
+        return sumOfEvenDigit;
     }
 }
