3610f94bd9333b62ea53ae2d5ce8090f8a20bdec
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-25 14:19:34 -0500

    feat: Create LargestPrime

---
+++ src/masterclass/learnprogramming/LargestPrime.java
@@ -0,0 +1,10 @@
+/* Section 5 : Coding Exercise 25: Largest Prime */
+
+package masterclass.learnprogramming;
+
+public class LargestPrime {
+
+    public static void main(String[] args) {
+
+    }
+}
