efcd550ba43971cd2a355c10a92e71043f6446e4
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-26 18:37:32 -0500

    feat: Complete InputCalculator

--- src/masterclass/learnprogramming/InputCalculator.java
+++ src/masterclass/learnprogramming/InputCalculator.java
@@ -1,7 +1,32 @@
 package masterclass.learnprogramming;
 
+import java.util.Scanner;
+
 public class InputCalculator {
     public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
 
+    public static void inputThenPrintSumAndAverage() {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        int sum = 0;
+        double avg = 0;
+        boolean keepGoing = true;
+        while (keepGoing) {
+            boolean isAnInt = scanner.hasNextInt();
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+                avg = (double) sum / counter;
+            } else {
+                keepGoing = false;
+            }
+            scanner.nextLine(); // handle end of line (enter key)
+        }
+
+        System.out.println("SUM = " + sum + " AVG = " + Math.round(avg));
+        scanner.close();
     }
 }
