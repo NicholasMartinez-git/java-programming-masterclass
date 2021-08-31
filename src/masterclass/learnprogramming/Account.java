67361bcd6336aac815beeaf7da9393b59b82367b
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-31 13:57:47 -0500

    feat: Create Account

---
+++ src/masterclass/learnprogramming/Account.java
@@ -0,0 +1,9 @@
+/* Section 6 : Constructors - Part 1: Challenge Exercise */
+
+package masterclass.learnprogramming;
+
+public class Account {
+    public static void main(String[] args) {
+
+    }
+}
