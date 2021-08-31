e949f6c7220aa94d177c1d9991d111c3713aaaba
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-30 20:41:01 -0500

    feat: Complete sumcalculator

--- src/masterclass/learnprogramming/sumcalculator/SimpleCalculator.java
+++ src/masterclass/learnprogramming/sumcalculator/SimpleCalculator.java
@@ -2,6 +2,53 @@ package masterclass.learnprogramming.sumcalculator;
 
 public class SimpleCalculator {
     public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("Add = " + calculator.getAdditionResult());
+        System.out.println("Subtract = " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("Multiply = " + calculator.getMultiplicationResult());
+        System.out.println("Division = " + calculator.getDivisionResult());
+    }
 
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if (firstNumber == 0 || secondNumber == 0) {
+            return 0.0;
+        } else {
+            return firstNumber / secondNumber;
+        }
     }
 }
