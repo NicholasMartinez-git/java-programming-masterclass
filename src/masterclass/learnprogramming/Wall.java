4c7544ab1f602bf9100cc8f06250265765fe45f0
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-09-01 14:00:22 -0500

    feat: Complete Wall

---
+++ src/masterclass/learnprogramming/Wall.java
@@ -0,0 +1,60 @@
+package masterclass.learnprogramming;
+
+public class Wall {
+    public static void main(String[] args) {
+        Wall wall = new Wall(5, 4);
+        System.out.println("Area = " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("Width = " + wall.getWidth());
+        System.out.println("Height = " + wall.getHeight());
+        System.out.println("Area = " + wall.getArea());
+    }
+
+    private double width;
+    private double height;
+
+    public Wall() {}
+
+    public Wall(double width, double height) {
+
+        if (width < 0) {
+            width = 0;
+        }
+
+        if (height < 0) {
+            height = 0;
+        }
+
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getArea() {
+        return height * width;
+    }
+}
