5d559885e2b1c3f963ce3e400cbc2bba9abe7916
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-09-01 15:26:54 -0500

    feat: Create Floor

---
+++ src/masterclass/learnprogramming/Floor.java
@@ -0,0 +1,9 @@
+/* Section 6 : Coding Exercise 33: Carpet Cost Calculator */
+
+package masterclass.learnprogramming;
+
+public class Floor {
+    public static void main(String[] args) {
+
+    }
+}
