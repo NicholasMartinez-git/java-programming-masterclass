db0b588ad17cda508f1eb56037477c17e18c8c10
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-09-01 22:57:59 -0500

    feat: Create Carpet
    Stored all classes for exercise in package carpetcostcalculator

--- src/masterclass/learnprogramming/Floor.java
+++
@@ -1,9 +0,0 @@
-/* Section 6 : Coding Exercise 33: Carpet Cost Calculator */
-
-package masterclass.learnprogramming;
-
-public class Floor {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/masterclass/learnprogramming/carpetcostcalculator/Carpet.java
@@ -0,0 +1,34 @@
+package masterclass.learnprogramming.carpetcostcalculator;
+
+public class Carpet {
+    public static void main(String[] args) {
+
+    }
+
+    private double cost;
+
+    public Carpet() {}
+
+    public Carpet(double cost) {
+
+        if (cost < 0) {
+            cost = 0;
+        }
+
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+
+        if (cost < 0) {
+            cost = 0;
+        }
+
+        this.cost = cost;
+    }
+
+}
