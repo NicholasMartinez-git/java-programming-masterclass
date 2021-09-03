c6d4a2b5d95bb23ff6269770a385fb6ac5aec679
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-09-02 20:45:49 -0500

    feat: Complete Calculator

--- src/masterclass/learnprogramming/carpetcostcalculator/Calculator.java
+++ src/masterclass/learnprogramming/carpetcostcalculator/Calculator.java
@@ -4,4 +4,18 @@ public class Calculator {
     public static void main(String[] args) {
 
     }
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator() {}
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
 }
