89766b702bb05ff7fde67704485f14ed850e702b
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-10-20 18:43:41 -0500

    feat: Complete SortedArray
    This commit was the accepted answer for the problem given.

--- src/masterclass/learnprogramming/SortedArray.java
+++ src/masterclass/learnprogramming/SortedArray.java
@@ -5,16 +5,16 @@ package masterclass.learnprogramming;
 import java.util.Scanner;
 
 public class SortedArray {
-    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Please enter how many integers you wish to sort?\r");
-        int userArrayLength = scanner.nextInt();
-        int[] userIntegers = getIntegers(userArrayLength);
-        printArray(sortIntegers(userIntegers));
+//        System.out.println("Please enter how many integers you wish to sort?\r");
+//        int userArrayLength = scanner.nextInt();
+//        int[] userIntegers = getIntegers(userArrayLength);
+//        printArray(sortIntegers(userIntegers));
     }
 
     public static int[] getIntegers(int number) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter " + number + " integer values.\r");
         int[] values = new int[number];
 
@@ -26,31 +26,37 @@ public class SortedArray {
     }
 
     public static int[] sortIntegers(int[] array) {
-        String sortConfirmation;
-
-        System.out.println("The program sorts the integers in ascending order by default.");
-        System.out.println("Would you like the integers to be in descending order? [y/n]");
-        sortConfirmation = scanner.next();
-        System.out.println("Sorting integers...");
+//        String sortConfirmation;
+//
+//        System.out.println("The program sorts the integers in ascending order by default.");
+//        System.out.println("Would you like the integers to be in descending order? [y/n]");
+//        sortConfirmation = scanner.next();
+//        System.out.println("Sorting integers...");
         int temp;
         // sorts and reverses the array
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
 
-                // the greater than or less than symbol determines whether the array will be ascending or descending
-                if (sortConfirmation.equalsIgnoreCase("y")) {
-                    if (array[i] < array[j]) {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                } else {
-                    if (array[i] > array[j]) {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
+                if (array[i] < array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                 }
+
+//                // the greater than or less than symbol determines whether the array will be ascending or descending
+//                if (sortConfirmation.equalsIgnoreCase("y")) {
+//                    if (array[i] < array[j]) {
+//                        temp = array[i];
+//                        array[i] = array[j];
+//                        array[j] = temp;
+//                    }
+//                } else {
+//                    if (array[i] > array[j]) {
+//                        temp = array[i];
+//                        array[i] = array[j];
+//                        array[j] = temp;
+//                    }
+//                }
             }
         }
 
